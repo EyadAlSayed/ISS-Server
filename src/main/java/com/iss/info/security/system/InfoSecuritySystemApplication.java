558c63e65f7c39823f8a69b0c32e196546af4595
mobile.digishi <mobile.digishi@gmail.com>
2022-12-13 15:50:49 +0200

    switch to 11 java version

---
+++ src/main/java/com/iss/info/security/system/InfoSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.iss.info.security.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoSecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoSecuritySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iss/info/security/system/InfoSecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iss.info.security.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoSecuritySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
