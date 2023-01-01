27f5c4c65c3761bd348143b5414ad5fd0869b8db
mobile.digishi <mobile.digishi@gmail.com>
2023-01-01 13:24:39 +0200

    fix certificate file

--- src/main/java/com/iss/info/security/system/repo/MessageRepo.java
+++ src/main/java/com/iss/info/security/system/repo/MessageRepo.java
@@ -2,7 +2,6 @@ package com.iss.info.security.system.repo;
 
 import com.iss.info.security.system.model.PersonMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
